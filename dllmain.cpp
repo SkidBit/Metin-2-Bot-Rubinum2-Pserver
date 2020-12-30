20d7c15b27d57f6b90fd29f5c23d631a1dac256f
SkidBit <76404877+SkidBit@users.noreply.github.com>
2020-12-30 05:02:41 +0100

    Update dllmain.cpp
    removed Blackbone, and updated the whole project basically

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,15 +1,13 @@
 #include "stdafx.h"
 #include "console.h"
 
-
-using namespace blackbone;
+using namespace std;
 #define show_console 1 //1 = show console ~ 0 = don't show console
+#define CHECK_BAD_PTR(x) if(IsBadReadPtr(this,sizeof(x))) return nullptr
+#define CHECK_BAD(x) if(IsBadReadPtr(this, sizeof(x))) return
+#define CHECK_BAD_NUM(x) if(IsBadReadPtr(this, sizeof(x))) return 0
 
-//Blackbone stuff
-Process proc;
-Process* procPointer;
-ModuleDataPtr pMainMod;
-ptr_t baseAdressMainMod;
+uintptr_t baseAdressMainMod;
 
 //typedefs for functions
 // slot logic 001, 101, 201 etc.
@@ -21,40 +19,40 @@ __pickupCloseFunc pickupCloseFunc;
 
 //MOB STRUCT
 //pointer structure: game + base offset, index(4*i), value to read
-//ptr_t offsetToMobList = 0x1CF8650;
+//uintptr_t offsetToMobList = 0x1CF8650;
 // 0x195EE38
-ptr_t offsetToMobList = 0x1C16B78;
+uintptr_t offsetToMobList = 0x1C16B78;
 //int
-ptr_t offsetToMobId = 0x244;
+uintptr_t offsetToMobId = 0x244;
 //floats
-ptr_t offsetToMobXPos = 0x2DC;
-ptr_t offsetToMobYPos = 0x2E0;
-ptr_t offsetToMobZPos = 0x2E4;
-ptr_t offsetToMobUniqueId = 0x1C54;
+uintptr_t offsetToMobXPos = 0x2DC;
+uintptr_t offsetToMobYPos = 0x2E0;
+uintptr_t offsetToMobZPos = 0x2E4;
+uintptr_t offsetToMobUniqueId = 0x1C54;
 //2 if alive 1 if dead
-ptr_t offsetToMobIsAlive = 0x730;
-ptr_t offsetToMobIsVisible = 0x340;
+uintptr_t offsetToMobIsAlive = 0x730;
+uintptr_t offsetToMobIsVisible = 0x340;
 
 //ATTACK MOB WITH ID
 //base offset
-ptr_t offsetToPlayerControlObject = 0x1CF8528;
+uintptr_t offsetToPlayerControlObject = 0x1CF8528;
 //int
-ptr_t offsetToAttackUID = 0x50;
+uintptr_t offsetToAttackUID = 0x50;
 
 //WALLHACK
 //base offset
-ptr_t offsetWallHackBase = 0x1CF853C;
+uintptr_t offsetWallHackBase = 0x1CF853C;
 //byte
-ptr_t offsetWallHackOne = 0xC;
-ptr_t offsetWallHackTwo = 0x1C5C;
+uintptr_t offsetWallHackOne = 0xC;
+uintptr_t offsetWallHackTwo = 0x1C5C;
 
 //PLAYER
-ptr_t offsetToPlayerBase = 0x1C6C354;
-ptr_t offsetToPlayerOne = 0xC;
+uintptr_t offsetToPlayerBase = 0x1C6C354;
+uintptr_t offsetToPlayerOne = 0xC;
 //float
-ptr_t offsetToPlayerXPos = 0x2DC;
-ptr_t offsetToPlayerYPos = 0x2E0;
-ptr_t offsetToPlayerZPos = 0x2E4;
+uintptr_t offsetToPlayerXPos = 0x2DC;
+uintptr_t offsetToPlayerYPos = 0x2E0;
+uintptr_t offsetToPlayerZPos = 0x2E4;
 
 //ids
 int metinIdStart = 8000;
@@ -63,25 +61,23 @@ int metinIdEnd = 8112;
 bool botRunning = false;
 bool firstLoop = true;
 
-std::vector<float> getPlayerPos(Process* proc);
-std::vector<float> getMobPos(Process* proc, ptr_t offsetInMoblist);
-ptr_t getOffsetOfClosestMetinStone(Process* proc, int scansize, std::vector<float> anchorPosition);
-int getMobUid(Process* proc, ptr_t offsetInMoblist);
-void playerAttackMobWithUid(Process* proc, ptr_t offsetInMobList);
-void enableWallhack(Process* proc);
-void disableWallhack(Process* proc);
-bool isPlayerAttackingMob(Process* proc);
-void resetPlayerAtatck(Process* proc);
+std::vector<float> getPlayerPos();
+std::vector<float> getMobPos(uintptr_t offsetInMoblist);
+uintptr_t getOffsetOfClosestMetinStone(int scansize, std::vector<float> anchorPosition);
+int getMobUid(uintptr_t offsetInMoblist);
+void playerAttackMobWithUid(uintptr_t offsetInMobList);
+void enableWallhack();
+void disableWallhack();
+bool isPlayerAttackingMob();
+void resetPlayerAtatck();
+uintptr_t findDMAAddy(uintptr_t ptr, vector<uintptr_t> offsets);
+bool IsBadReadPtr(void* p);
 
 
 DWORD WINAPI MainThread(LPVOID param) {
-	proc.Attach(GetCurrentProcessId());
-	procPointer = &proc;
+	baseAdressMainMod = (uintptr_t)GetModuleHandle(NULL);
 
-	pMainMod = proc.modules().GetMainModule();
-	baseAdressMainMod = pMainMod->baseAddress;
-
-	std::cout << "Main module base address: 0x" << std::hex << baseAdressMainMod << std::endl;
+	cout << "Main module base address: 0x" << std::hex << baseAdressMainMod << endl;
 
 	// set the address of the function (relative to modbase)
 	useItemFunc = (__useItemFunc)(baseAdressMainMod + 0x1695B0);
@@ -90,7 +86,7 @@ DWORD WINAPI MainThread(LPVOID param) {
 	// useItemFunc(*(void**)0x20F8560, 0x101);
 
 	std::vector<float> anchorPosition;
-	ptr_t offsetClosestStone;
+	uintptr_t offsetClosestStone;
 	std::chrono::steady_clock::time_point timerStart;
 	std::chrono::steady_clock::time_point timerRound;
 
@@ -102,40 +98,41 @@ DWORD WINAPI MainThread(LPVOID param) {
 		if (botRunning) {
 
 			if (firstLoop) {
-				std::cout << "FirstLoop setup is run..." << std::endl;
+				cout << "FirstLoop setup is run..." << endl;
+				Sleep(5000);
 				// enable wallhack
-				enableWallhack(procPointer);
-				std::cout << "WH enabled" << std::endl;
+				enableWallhack();
+				cout << "WH enabled" << endl;
 				// get Anchor position
-				anchorPosition = getPlayerPos(procPointer);
-				std::cout << "Anchor position set" << std::endl;
+				anchorPosition = getPlayerPos();
+				cout << "Anchor position set" << endl;
 
 				firstLoop = false;
 			}
 
-			if (!isPlayerAttackingMob(procPointer)) {
+			if (!isPlayerAttackingMob()) {
 				Sleep(1000);
 				// pickup and wait for short amount
 				pickupCloseFunc(*(void**)0x020F8528);
-				std::cout << "Picked up loot" << std::endl;
+				cout << "Picked up loot" << endl;
 				// sleeps are neccessary because stone will be read as alive short time after death
 				Sleep(3000);
 
 				// get closest stone
-				offsetClosestStone = getOffsetOfClosestMetinStone(procPointer, 1500, anchorPosition);
+				offsetClosestStone = getOffsetOfClosestMetinStone(1500, anchorPosition);
 				// attack stone
-				playerAttackMobWithUid(procPointer, offsetClosestStone);
+				playerAttackMobWithUid(offsetClosestStone);
 				// start timing to detect attackid bug
 				timerStart = std::chrono::steady_clock::now();
-				std::cout << "Attacking NOW..." << std::endl;
+				cout << "Attacking NOW..." << endl;
 			}
 
 			timerRound = std::chrono::steady_clock::now();
 
 			if (std::chrono::duration_cast<std::chrono::seconds>(timerRound - timerStart).count() > 60) {
 				// 60 seconds ago we started attacking and stone still lives, game is buggy
-				resetPlayerAtatck(procPointer);
-				std::cout << "Game bugged out, reset issued" << std::endl;
+				resetPlayerAtatck();
+				cout << "Game bugged out, reset issued" << endl;
 			}
 
 		}
@@ -145,7 +142,7 @@ DWORD WINAPI MainThread(LPVOID param) {
 
 		if (GetAsyncKeyState(VK_F1) & 1) {
 			botRunning = !botRunning;
-			std::cout << "BOT TOGGLED" << std::endl;
+			cout << "BOT TOGGLED" << endl;
 			Sleep(200);
 		}
 
@@ -167,8 +164,6 @@ DWORD WINAPI MainThread(LPVOID param) {
 }
 
 
-
-
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
@@ -188,37 +183,49 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	return TRUE;
 }
 
-bool isPlayerAttackingMob(Process* proc) {
-	int mobUid;
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToPlayerControlObject, offsetToAttackUID }, mobUid);
+bool isPlayerAttackingMob() {
+	int* mobUidAddress = (int*)findDMAAddy(baseAdressMainMod + offsetToPlayerControlObject, { offsetToAttackUID });
+	
+	if (IsBadReadPtr(mobUidAddress)) {
+		return 0;
+	}
 
-	//std::cout << "Checking if player is attacking something. Value: " << std::dec << mobUid << std::endl;
-
-	return mobUid != 0;
+	//cout << "Checking if player is attacking something. Value: " << std::dec << mobUid << endl;
+	return *mobUidAddress != 0;
 }
 
-ptr_t getOffsetOfClosestMetinStone(Process* proc, int scansize, std::vector<float> anchorPosition) {
-	std::cout << "Searching for metin stones now..." << std::endl;
-	ptr_t offsetOfClosestStone;
+uintptr_t getOffsetOfClosestMetinStone(int scansize, std::vector<float> anchorPosition) {
+	cout << "Searching for metin stones now..." << endl;
+	uintptr_t offsetOfClosestStone = 0x0;
 	float distanceToClosestStone = INFINITY;
 
 	int tempMobId;
+	int * tempMobIdAddress;
 	std::vector<float> tempMobPos;
 	float tempDistanceToClosestStone;
 
 
 	for (int i = 0; i < scansize; i++) {
-		ptr_t currentOffsetToEntity = i * 4;
+		uintptr_t currentOffsetToEntity = i * 4;
 
-		proc->memory().Read({ (proc->modules().GetMainModule()->baseAddress + offsetToMobList), currentOffsetToEntity, offsetToMobId }, tempMobId);
+
+
+		tempMobIdAddress = (int*)findDMAAddy(baseAdressMainMod + offsetToMobList, { currentOffsetToEntity, offsetToMobId });
+		if (IsBadReadPtr(tempMobIdAddress)) {
+			tempMobId = 0;
+		}
+		else {
+			tempMobId = *tempMobIdAddress;
+		}
+
 
 		// check if mob is metin stone
 		if (tempMobId >= metinIdStart && tempMobId <= metinIdEnd) {
-			std::cout << "Stone found, mobID: " << std::dec << tempMobId << std::endl;
-			tempMobPos = getMobPos(proc, currentOffsetToEntity);
+			cout << "Stone found, mobID: " << std::dec << tempMobId << endl;
+			tempMobPos = getMobPos(currentOffsetToEntity);
 			//calculate distance to anchor position
 			tempDistanceToClosestStone = sqrt(pow(anchorPosition.at(0) - tempMobPos.at(0), 2) + pow(anchorPosition.at(1) - tempMobPos.at(1), 2));
-			std::cout << "Distance to stone: " << std::dec << tempDistanceToClosestStone << std::endl;
+			cout << "Distance to stone: " << std::dec << tempDistanceToClosestStone << endl;
 			if (tempDistanceToClosestStone < distanceToClosestStone) {
 				distanceToClosestStone = tempDistanceToClosestStone;
 				offsetOfClosestStone = currentOffsetToEntity;
@@ -230,72 +237,107 @@ ptr_t getOffsetOfClosestMetinStone(Process* proc, int scansize, std::vector<floa
 	return offsetOfClosestStone;
 }
 
-void enableWallhack(Process* proc) {
-	proc->memory().Write({ proc->modules().GetMainModule()->baseAddress + offsetWallHackBase, offsetWallHackOne, offsetWallHackTwo }, (byte)0x1);
+void enableWallhack() {
+	BYTE* wallHackAddress = (BYTE*)findDMAAddy(baseAdressMainMod + offsetWallHackBase, { offsetWallHackOne, offsetWallHackTwo });
+	*wallHackAddress = 0x1;
 }
 
-void disableWallhack(Process* proc) {
-	proc->memory().Write({ proc->modules().GetMainModule()->baseAddress + offsetWallHackBase, offsetWallHackOne, offsetWallHackTwo }, (byte)0x0);
+void disableWallhack() {
+	BYTE* wallHackAddress = (BYTE*)findDMAAddy(baseAdressMainMod + offsetWallHackBase, { offsetWallHackOne, offsetWallHackTwo });
+	*wallHackAddress = 0x0;
 }
 
-void playerAttackMobWithUid(Process* proc, ptr_t offsetInMobList) {
+void playerAttackMobWithUid(uintptr_t offsetInMobList) {
 
-	//make mob visible
-	proc->memory().Write({ proc->modules().GetMainModule()->baseAddress + offsetToMobList, offsetInMobList, offsetToMobIsVisible }, 0x1);
+	// make mob visible
+	BYTE* isVisibleAddress = (BYTE*)findDMAAddy(baseAdressMainMod + offsetToMobList, { offsetInMobList, offsetToMobIsVisible });
+	*isVisibleAddress = 0x1;
 
-	int mobUid = getMobUid(proc, offsetInMobList);
+	int mobUid = getMobUid(offsetInMobList);
 
-	proc->memory().Write({ proc->modules().GetMainModule()->baseAddress + offsetToPlayerControlObject, offsetToAttackUID }, mobUid);
+	// attack mob
+	int* attackMobAddress = (int*)findDMAAddy(baseAdressMainMod + offsetToPlayerControlObject, { offsetToAttackUID });
+	*attackMobAddress = mobUid;
 }
 
-int getMobUid(Process* proc, ptr_t offsetInMoblist) {
-	int mobUid;
+int getMobUid(uintptr_t offsetInMoblist) {
+	int* mobUidAddress = (int*)findDMAAddy(baseAdressMainMod + offsetToMobList, { offsetInMoblist,  offsetToMobUniqueId });
 
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToMobList, offsetInMoblist, offsetToMobUniqueId }, mobUid);
-
-	return mobUid;
+	return *mobUidAddress;
 }
 
-std::vector<float> getMobPos(Process* proc, ptr_t offsetInMoblist) {
+std::vector<float> getMobPos(uintptr_t offsetInMoblist) {
 	std::vector<float> mobPos;
-	float xPos;
-	float yPos;
-	float zPos;
+	float* xPosAddress;
+	float* yPosAddress;
+	float* zPosAddress;
 
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToMobList, offsetInMoblist, offsetToMobXPos }, xPos);
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToMobList, offsetInMoblist, offsetToMobYPos }, yPos);
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToMobList, offsetInMoblist, offsetToMobZPos }, zPos);
+	xPosAddress = (float*)findDMAAddy(baseAdressMainMod + offsetToMobList, { offsetInMoblist,  offsetToMobXPos });
+	yPosAddress = (float*)findDMAAddy(baseAdressMainMod + offsetToMobList, { offsetInMoblist,  offsetToMobYPos });
+	zPosAddress = (float*)findDMAAddy(baseAdressMainMod + offsetToMobList, { offsetInMoblist,  offsetToMobZPos });
+
 
 	//invert yPos since its stored as negative value
-	yPos = yPos * -1;
 
-	mobPos.push_back(xPos);
-	mobPos.push_back(yPos);
-	mobPos.push_back(zPos);
+	mobPos.push_back(*xPosAddress);
+	mobPos.push_back(*yPosAddress * -1);
+	mobPos.push_back(*zPosAddress);
 
 	return mobPos;
 }
 
-std::vector<float> getPlayerPos(Process* proc) {
+std::vector<float> getPlayerPos() {
 	std::vector<float> playerPos;
-	float xPos;
-	float yPos;
-	float zPos;
+	float* xPosAddress;
+	float* yPosAddress;
+	float* zPosAddress;
 
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToPlayerBase, offsetToPlayerOne, offsetToPlayerXPos }, xPos);
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToPlayerBase, offsetToPlayerOne, offsetToPlayerYPos }, yPos);
-	proc->memory().Read({ proc->modules().GetMainModule()->baseAddress + offsetToPlayerBase, offsetToPlayerOne, offsetToPlayerZPos }, zPos);
+	xPosAddress = (float*)findDMAAddy(baseAdressMainMod + offsetToPlayerBase, { offsetToPlayerOne,  offsetToPlayerXPos });
+	yPosAddress = (float*)findDMAAddy(baseAdressMainMod + offsetToPlayerBase, { offsetToPlayerOne,  offsetToPlayerYPos });
+	zPosAddress = (float*)findDMAAddy(baseAdressMainMod + offsetToPlayerBase, { offsetToPlayerOne,  offsetToPlayerZPos });
+
 
 	//invert yPos since its stored as negative value
-	yPos = yPos * -1;
 
-	playerPos.push_back(xPos);
-	playerPos.push_back(yPos);
-	playerPos.push_back(zPos);
+	playerPos.push_back(*xPosAddress);
+	playerPos.push_back(*yPosAddress * -1);
+	playerPos.push_back(*zPosAddress);
 
 	return playerPos;
 }
 
-void resetPlayerAtatck(Process* proc) {
-	proc->memory().Write({ proc->modules().GetMainModule()->baseAddress + offsetToPlayerControlObject, offsetToAttackUID }, 0x0);
+void resetPlayerAtatck() {
+
+	int* playerAttackAddress = (int*)findDMAAddy(baseAdressMainMod + offsetToPlayerControlObject, { offsetToAttackUID });
+
+	*playerAttackAddress = 0x0;
+}
+
+uintptr_t findDMAAddy(uintptr_t ptr, vector<uintptr_t> offsets)
+{
+	uintptr_t addr = ptr;
+
+	for (unsigned int i = 0; i < offsets.size(); ++i)
+	{
+		addr = *(uintptr_t*)addr;
+		addr += offsets[i];
+	}
+
+
+	return addr;
+}
+
+bool IsBadReadPtr(void* p)
+{
+	MEMORY_BASIC_INFORMATION mbi = { 0 };
+	if (::VirtualQuery(p, &mbi, sizeof(mbi)))
+	{
+		DWORD mask = (PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY);
+		bool b = !(mbi.Protect & mask);
+		// check the page is not a guard page
+		if (mbi.Protect & (PAGE_GUARD | PAGE_NOACCESS)) b = true;
+
+		return b;
+	}
+	return true;
 }
