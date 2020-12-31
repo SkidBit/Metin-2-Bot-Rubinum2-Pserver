44d4973e4eb952d594c0253a05d3873264c9a63c
Johanna <76404877+SkidBit@users.noreply.github.com>
2020-12-31 12:27:34 +0100

    added patterscans

--- mem.h
+++ mem.h
@@ -4,4 +4,7 @@
 namespace mem {
 	uintptr_t findDMAAddy(uintptr_t ptr, std::vector<uintptr_t> offsets);
 	bool IsBadReadPtr(void* p);
+	char* ScanBasic(char* pattern, char* mask, char* begin, intptr_t size);
+	char* ScanInternal(char* pattern, char* mask, char* begin, intptr_t size);
+	char* ScanModIn(char* pattern, char* mask, std::string modName);
 }
