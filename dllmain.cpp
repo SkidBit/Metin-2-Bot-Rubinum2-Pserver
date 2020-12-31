747c082bf0fe72e124349b62089e473aa5418734
Johanna <76404877+SkidBit@users.noreply.github.com>
2020-12-31 12:30:21 +0100

    added first patternscans

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,12 +1,21 @@
 #include "stdafx.h"
 #include "console.h"
 #include "mem.h"
-#include "offsets.h"
+#include "constants.h"
 #include "game.h"
 
 using namespace std;
 #define show_console 1 //1 = show console ~ 0 = don't show console
 
+//Function
+//Address of signature = rbclient.exe + 0x000CAD10
+//"\x55\x8B\xEC\x6A\x00\x68\x00\x00\x00\x00\x64\x00\x00\x00\x00\x00\x00\x00\x00\x00\x53\x56\x57\xA1\x00\x00\x00\x00\x00\x00\x00\x00\x45\x00\x64\x00\x00\x00\x00\x00\x00\x00\x00\x00\xF0\x8D\x00\x00\x8B\x01", "xxxx?x????x?????????xxxx????????x?x?????????xx??xx"
+//"55 8B EC 6A ? 68 ? ? ? ? 64 ? ? ? ? ? ? ? ? ? 53 56 57 A1 ? ? ? ? ? ? ? ? 45 ? 64 ? ? ? ? ? ? ? ? ? F0 8D ? ? 8B 01"
+
+//Classpointer, skip 2 bytes and then copy address
+//Address of signature = rbclient.exe + 0x000CD0E0
+//"\x8B\x0D\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xE9\x00\x00\x00\x00\x55\x8B\xEC\x51\x8D\x45\x00\x50\x6A\x00\xFF\x75\x00\xE8\x00\x00\x00\x00\x83\xC4\x00\x84\xC0\x75\x00\xE8\x00\x00\x00\x00\x8B\xE5\x5D\xC3\xFF\x75\x00\x8B\x0D\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x8B\xE5\x5D\xC3\xCC\xCC\xCC\xCC\xCC\xCC\xCC\x55\x8B\xEC\x51\x8D\x45\x00\x50\x6A\x00\xFF\x75\x00\xE8\x00\x00\x00\x00\x83\xC4\x00\x84\xC0\x75\x00\xE8\x00\x00\x00\x00\x8B\xE5\x5D\xC3\x83\x7D\xFC", "xx????x????x????xxxxxx?xx?xx?x????xx?xxx?x????xxxxxx?xx????x????x????xxxxxxxxxxxxxxxxx?xx?xx?x????xx?xxx?x????xxxxxxx"
+//"8B 0D ? ? ? ? E8 ? ? ? ? E9 ? ? ? ? 55 8B EC 51 8D 45 ? 50 6A ? FF 75 ? E8 ? ? ? ? 83 C4 ? 84 C0 75 ? E8 ? ? ? ? 8B E5 5D C3 FF 75 ? 8B 0D ? ? ? ? E8 ? ? ? ? E8 ? ? ? ? 8B E5 5D C3 CC CC CC CC CC CC CC 55 8B EC 51 8D 45 ? 50 6A ? FF 75 ? E8 ? ? ? ? 83 C4 ? 84 C0 75 ? E8 ? ? ? ? 8B E5 5D C3 83 7D FC"
 
 typedef void(__thiscall* __pickupCloseFunc)(void* classPointer);
 __pickupCloseFunc pickupCloseFunc;
@@ -18,10 +27,20 @@ uintptr_t baseAdressMainMod;
 
 DWORD WINAPI MainThread(LPVOID param) {
 	baseAdressMainMod = (uintptr_t)GetModuleHandle(NULL);
-
+	cout << "-----DEBUGGING-----" << endl;
 	cout << "Main module base address: 0x" << hex << baseAdressMainMod << endl;
 
-	pickupCloseFunc = (__pickupCloseFunc)(baseAdressMainMod + 0xCAD10);
+	uintptr_t pickupFunctionAddress = (uintptr_t)mem::ScanModIn((char*)pickupFunctionPattern, (char*)pickupFunctionMask, "rbclient.exe");
+	uintptr_t pickupFunctionClassPointerFunctionAddress = (uintptr_t)mem::ScanModIn((char*)pickupFunctionClassPointerFunctionPattern, (char*)pickupFunctionClassPointerFunctionMask, "rbclient.exe");
+
+	cout << "Pickup function address: 0x" << hex << pickupFunctionAddress << endl;
+	cout << "Pickup classpointer function address: 0x" << hex << pickupFunctionClassPointerFunctionAddress << endl;
+
+	uintptr_t pickupFunctionClassPointer = *(uintptr_t*)(pickupFunctionClassPointerFunctionAddress + pickupFunctionOffsetToClassPointer);
+
+	cout << "Pickup classpointer address: 0x" << hex << pickupFunctionClassPointer << endl;
+	cout << "-----DEBUGGING-----" << endl;
+	pickupCloseFunc = (__pickupCloseFunc)(pickupFunctionAddress);
 	// pickupCloseFunc(*(void**)0x020F8528);
 
 	vector<float> anchorPosition;
@@ -30,7 +49,6 @@ DWORD WINAPI MainThread(LPVOID param) {
 	chrono::steady_clock::time_point timerRound;
 
 	while (true) {
-
 
 		// bot loop
 		// check if bot enabled
@@ -52,7 +70,7 @@ DWORD WINAPI MainThread(LPVOID param) {
 			if (!game::isPlayerAttackingMob()) {
 				Sleep(1000);
 				// pickup and wait for short amount
-				pickupCloseFunc(*(void**)0x020F8528);
+				pickupCloseFunc(*(void**)pickupFunctionClassPointer);
 				cout << "Picked up loot" << endl;
 				// sleeps are neccessary because stone will be read as alive short time after death
 				Sleep(3000);
