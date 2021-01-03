080d0cb6182b4b8a4f185673c42bb8913f15ccc3
Johanna <76404877+SkidBit@users.noreply.github.com>
2021-01-03 03:35:07 +0100

    Update console.h

--- SteinBoxer/console.h
+++ SteinBoxer/console.h
@@ -10,7 +10,7 @@ void console(LPVOID hModule) { //console assignment
 
 		AttachConsole(GetProcessId((hModule)));
 
-		SetConsoleTitle(L"SteinBoxer v1.4");
+		SetConsoleTitle(L"SteinBoxer v1.5");
 
 		// disable close button
 		EnableMenuItem(GetSystemMenu(GetConsoleWindow(), FALSE), SC_CLOSE, MF_DISABLED);
